44df5bf4e693ceecf3e79c8c099c18282f20bcf3
fitorehalimi <44590821+fitorehalimi@users.noreply.github.com>
2019-06-10 21:28:38 +0200

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,81 @@
+package DatabaseConn;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Menu extends Application {
+	Stage window;
+	BorderPane layout;
+
+	public static void main (String[] args) {
+		launch(args);
+	}
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+	  window = primaryStage;
+	  window.setTitle("Payroll And Contract Management");
+	  
+	  //File menu 
+	  Menu fileMenu = new Menu("File");
+	  fileMenu.getItems().add(new MenuItem("New..."));
+	  fileMenu.getItems().add(new MenuItem("Open..."));
+	  fileMenu.getItems().add(new MenuItem("Save..."));
+	  fileMenu.getItems().add(new SeparatorMenuItem());
+	  fileMenu.getItems().add(new MenuItem("Settings..."));
+	  fileMenu.getItems().add(new SeparatorMenuItem());
+	  fileMenu.getItems().add(new MenuItem("Exit..."));
+	  
+	  //Edit menu
+	  Menu editMenu = new Menu("_Edit");
+	  editMenu.getItems().add(new MenuItem("Out"));
+	  editMenu.getItems().add(new MenuItem("Copy"));
+	  editMenu.getItems().add(new MenuItem("Paste"));
+	  
+	  //Search menu
+	  Menu search = new Menu("Search");
+	  
+	  //Add menu
+	  Menu AddMenu = new Menu("Add");
+	  AddMenu.getItems().add(new MenuItem(""));
+	  AddMenu.getItems().add(new MenuItem(""));
+	  AddMenu.getItems().add(new MenuItem(""));
+	 
+	  //Language menu
+	  Menu LangMenu = new Menu("Language");
+	  ToggleGroup LangToggle = new ToggleGroup();
+	  
+	  RadioMenuItem English = new RadioMenuItem("English");
+	  RadioMenuItem Albanian = new RadioMenuItem("Albanian");
+	  
+	  English.setToggleGroup(LangToggle);
+	  Albanian.setToggleGroup(LangToggle);
+	  
+	  LangMenu.getItems().addAll(English, Albanian);
+	  
+	  //Help menu
+	  Menu helpMenu = new Menu("Help");
+	  helpMenu.getItems().add(new MenuItem("Welcome"));
+	  helpMenu.getItems().add(new MenuItem("Help Content"));
+	  helpMenu.getItems().add(new MenuItem("Show Contextual Help"));
+	  helpMenu.getItems().add(new MenuItem("Tip of the day"));
+	  helpMenu.getItems().add(new MenuItem("Tips and Tricks"));
+	  helpMenu.getItems().add(new MenuItem("Contribute"));
+	  
+	  //Main menu bar
+	  MenuBar menuBar = new MenuBar();
+	  menuBar.getMenus().addAll(fileMenu, editMenu, search, AddMenu,LangMenu, helpMenu);
+	  
+	  layout = new BorderPane();
+	  layout.setTop(menuBar);
+	 
+	  
+	  Scene scene = new Scene(layout, 600, 400);
+	  window.setScene(scene);
+	  window.show();
+	}
+
+}
