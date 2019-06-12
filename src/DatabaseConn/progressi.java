e54d46ae856f7baf4ec3b36814f285227ea1de02
ElbaKurteshi <44671780+ElbaKurteshi@users.noreply.github.com>
2019-06-12 20:38:55 +0200

    Add files via upload

---
+++ src/DatabaseConn/progressi.java
@@ -0,0 +1,33 @@
+package ProjektiNew;
+
+import javafx.application.Application; 
+import javafx.scene.Scene; 
+import javafx.scene.control.*; 
+import javafx.scene.layout.*; 
+import java.io.*; 
+import javafx.event.ActionEvent; 
+import javafx.event.EventHandler; 
+import javafx.scene.control.Label; 
+import javafx.stage.Stage; 
+import java.net.*; 
+public class progressi extends Application { 
+  
+    static double ii = 0; 
+   
+    public void start(Stage s) throws Exception 
+    { 
+        
+        ProgressIndicator pb = new ProgressIndicator(); 
+       
+        TilePane r = new TilePane(); 
+  
+        EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() { 
+            public void handle(ActionEvent e) 
+            { 
+                
+                ii += 0.1; 
+                pb.setProgress(ii); 
+               } 
+  
+        }; 
+  
