3b1d93d322fa39c32f1954243ab1e9f499f557bc
arianfazliu <44971000+arianfazliu@users.noreply.github.com>
2019-06-12 13:34:46 +0200

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -12,4 +12,19 @@ public class DBConnection {
 	private final static String username = "root";
 	private final static String password = "nihon123";
 	
-	
+	public static Connection getConnection() {
+		if(dbConnection == null) {
+			try {
+				Class.forName("com.mysql.cj.jdbc.Driver");
+				dbConnection = 
+						DriverManager.getConnection("jdbc:mysql://" + host+ "/" + dbName, username, password);
+				
+			} catch(Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		
+		return dbConnection;
+	}
+		
+}
